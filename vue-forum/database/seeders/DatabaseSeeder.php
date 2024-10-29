8794a5f91a78632b25ebcaf1ef8045fbeb0bed48
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-10-29 21:30:21 +0100

    Update seeders

--- vue-forum/database/seeders/DatabaseSeeder.php
+++ vue-forum/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -31,27 +33,27 @@ class DatabaseSeeder extends Seeder
             'Analiza danych',
             'Inżynieria oprogramowania',
             'Obliczenia w chmurze',
-            'Handel elektroniczny'
+            'Handel elektroniczny',
         ])->map(function ($name) {
             return Category::create(['name' => $name]);
         });
 
         // Create tags
         $tags = collect([
-            'Nauka', 
+            'Nauka',
             'Potrzebna pomoc',
-            'Edukacja', 
-            'Programowanie', 
-            'PHP', 
-            'Laravel', 
-            'JavaScript', 
-            'Tworzenie stron', 
-            'Backend', 
-            'Frontend', 
-            'Bazy danych', 
-            'API', 
-            'Kodowanie', 
-            'Technologia'
+            'Edukacja',
+            'Programowanie',
+            'PHP',
+            'Laravel',
+            'JavaScript',
+            'Tworzenie stron',
+            'Backend',
+            'Frontend',
+            'Bazy danych',
+            'API',
+            'Kodowanie',
+            'Technologia',
         ])->map(function ($name) {
             return Tag::create(['name' => $name]);
         });
@@ -60,9 +62,9 @@ class DatabaseSeeder extends Seeder
         $posts = [];
         foreach ($users as $user) {
             $posts[] = $user->posts()->createMany([
-                ['title' => 'Post 1', 'content' => 'Content of Post 1'],
-                ['title' => 'Post 2', 'content' => 'Content of Post 2'],
-                ['title' => 'Post 3', 'content' => 'Content of Post 3'],
+                ['title' => 'Jak mogę zachować stan pomiędzy renderowaniami w komponentach funkcyjnych React?', 'content' => 'Używam komponentów funkcyjnych w React z hookiem useState, ale mam problem z utrzymaniem stanu przy ponownym renderowaniu komponentu. Na przykład, gdy pobieram dane i zapisuję je w zmiennej stanu, czasami stan ten nieoczekiwanie się resetuje'],
+                ['title' => 'Jak rozwiązać problem CORS przy połączeniu z API w JavaScript?', 'content' => 'Próbuję połączyć się z zewnętrznym API za pomocą fetch w mojej aplikacji JavaScript, ale ciągle otrzymuję błąd CORS (Cross-Origin Resource Sharing)'],
+                ['title' => 'Jaka jest różnica między == a === w JavaScript', 'content' => 'Nie rozumiem różnicy między operatorem == a === w JavaScript. Przykładowo, kiedy używam ==, porównanie daje wynik prawdziwy, ale gdy używam ===, wynik jest fałszywy.'],
             ]);
         }
 
@@ -73,9 +75,9 @@ class DatabaseSeeder extends Seeder
                 $post->tags()->attach($tags->random()->id);
 
                 // Create comments for each post
-                for ($i = 0; $i < 2; $i++) {
+                for ($i = 1; $i <= 2; $i++) {
                     Comment::create([
-                        'content' => "Comment $i on {$post->title}",
+                        'content' => "Komentarz $i do pytania: {$post->title}",
                         'user_id' => $users->random()->id,
                         'post_id' => $post->id,
                     ]);
